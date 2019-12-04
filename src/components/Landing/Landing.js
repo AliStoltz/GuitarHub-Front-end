9647233aaaf176aec173a733dfdc7e04a476b291
AliStoltz <ali.mstoltz@gmail.com>
2019-12-03 17:51:24 -0800

    built out components

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Landing.css';
+import { directive } from '@babel/types';
+
+const Landing = (props) => {
+  return(
+    <div className="landing-container">
+      <h1>Julian sux!</h1>
+    </div>
+  )
+};
+
+export default Landing;
