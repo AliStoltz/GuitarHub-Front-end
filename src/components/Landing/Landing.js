b535912d872392d176f74972418e1ae3a1e8135f
AliStoltz <ali.mstoltz@gmail.com>
2019-12-04 19:09:41 -0800

    bootstrap isnt working

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
 import './Landing.css';
+import hero from './hero.jpg';
+import LandingCarousel from '../../containers/LandingCarouselContainer/LandingCarousel';
 
 const Landing = (props) => {
   return(
     <div className="landing-container">
-      <h1>Julian sux!</h1>
+      <div className="hero">
+          <img src={hero} alt="hero"/>
+      </div>
+      <div>
+        <LandingCarousel />
+      </div>
     </div>
   )
 };
---
+++ src/containers/LandingCarouselContainer/LandingCarousel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingCarousel.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "mdbreact/dist/css/mdb.css";
+
+const LandingCarousel = () => (
+  <div className="carousel-item">
+    <img src="https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/dogs_1280p_0.jpg?itok=4t_1_fSJ" alt="..."/>
+    <div className="carousel-caption d-none d-md-block">
+      <h5>...</h5>
+      <p>...</p>
+    </div>
+  </div>
+);
+
+export default LandingCarousel;
