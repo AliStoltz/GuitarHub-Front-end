09ab2ed0f7727a00c7f72b7398ec1c7767e7258a
AliStoltz <ali.mstoltz@gmail.com>
2019-12-04 19:32:49 -0800

    axios call for guitar data

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Landing.css';
 import hero from './hero.jpg';
 import LandingCarousel from '../../containers/LandingCarouselContainer/LandingCarousel';
+import axios from 'axios';
 
-const Landing = (props) => {
+class Landing extends Component  {
+
+  state = {
+    guitars: [],
+  }
+
+  componentDidMount() {
+    this.test();
+  };
+
+  test = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/guitars/all`)
+    .then((res) => {
+      console.log(res.data);
+      this.setState({
+        guitars: res.data.data
+      })
+    })
+    .catch((err) => console.log(err));
+  };
+
+  render() {
+    const guitars = this.state.guitars.map(guitar => {
+      return (
+        <div>
+          <h2 className="ahhh"> {guitar.name}</h2>
+          <img className="guitarImg"src={guitar.photo} alt="fuck"/>
+        </div>
+      )
+    })
+
   return(
     <div className="landing-container">
       <div className="hero">
           <img src={hero} alt="hero"/>
       </div>
       <div>
-        <LandingCarousel />
+        {guitars}
       </div>
     </div>
   )
+  }
 };
 
 export default Landing;
